92b5a3caedc17a3c8f8ea0165bd55156c96efb27
eyup çelebi <eyupcelebi78@gmail.com>
2020-05-03 19:23:52 +0300

    first commit

---
+++ FirstProject/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstProject
+{
+	/// <summary>
+	/// MainWindow.xaml etkileşim mantığı
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<Bilgiler> liste;
+		public MainWindow()
+		{
+			InitializeComponent();
+			liste = new List<Bilgiler>()
+			{
+				new Bilgiler() {Ad="Eyüp",Soyad="Çelebi",Cinsiyet="Erkek",OgrenciNo="181130058",Bolum="Bilgisayar Programcılığı" }
+
+			};
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			tablo.ItemsSource = liste;
+		}
+
+		private void btnAdd_Click(object sender, RoutedEventArgs e)
+		{
+			Edit edit1 = new Edit();
+			edit1.Show();
+			this.Hide();
+		}
+	}
+
+	public class Bilgiler
+	{
+		public string Ad { get; set; }
+		public string Soyad { get; set; }
+		public string Cinsiyet { get; set; }
+		public string OgrenciNo { get; set; }
+		public string Bolum { get; set; }
+
+	}
+}
---
+++ FirstProject/StudentAdded.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstProject
+{
+    /// <summary>
+    /// Edit.xaml etkileşim mantığı
+    /// </summary>
+    public partial class Edit : Window
+    {
+        List<Bilgiler> bilgiler = new List<Bilgiler>();
+        public Edit()
+        {
+            InitializeComponent();
+            lblErrorMessage.Visibility = Visibility.Hidden;
+
+
+        }
+
+        public void errorMessage (string isim)
+        {
+            lblErrorMessage.Visibility = Visibility.Visible;
+            lblErrorMessage.Content = isim+" Alanı boş bırakılamaz";
+        }
+
+        int syc = 0;
+        private void btnKaydet_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtAd.Text == "")
+            {
+                MessageBox.Show("İsim alanı boş geçilemez…");
+                errorMessage("İsim");
+
+            }
+
+            else if (txtSoyad.Text == "")
+            {
+                MessageBox.Show("Soyisim alanı boş geçilemez…");
+                errorMessage("Soyisim");
+            }
+
+            else if (cmbCinsiyet.Text == "")
+            {
+                MessageBox.Show("Cinsiyet alanı boş geçilemez…");
+                errorMessage("Cinsiyet");
+            }
+
+            else if (txtOgrenciNo.Text == "")
+            {
+                MessageBox.Show("Öğrenci No alanı boş geçilemez…");
+                errorMessage("Öğrenci No");
+            }
+
+            else if (cmbBolum.Text == "")
+            {
+                MessageBox.Show("Bölüm bilgisi boş geçilemez…");
+                errorMessage("Bölüm");
+            }
+
+            else
+            {
+
+                
+                Bilgiler kullanici = new Bilgiler();
+                kullanici.Ad = txtAd.Text;
+                kullanici.Soyad = txtSoyad.Text;
+                kullanici.Cinsiyet = cmbCinsiyet.Text;
+                kullanici.OgrenciNo = txtOgrenciNo.Text;
+                kullanici.Bolum = cmbBolum.Text;
+                bilgiler.Add(kullanici);
+                tablo2.Items.Add(kullanici);
+                cmbIndex.Items.Add(kullanici.Ad);
+                lblErrorMessage.Visibility = Visibility.Hidden;
+            }
+
+        }
+
+        private void cmbIndex_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var secilen = cmbIndex.SelectedIndex;
+            MessageBox.Show("Ad : "+bilgiler[secilen].Ad + "\tSoyad : " + bilgiler[secilen].Soyad + "\tCinsiyet : " + bilgiler[secilen].Cinsiyet + "\tOgrenciNo : " + bilgiler[secilen].OgrenciNo + "\tBolum : " + bilgiler[secilen].Bolum);
+        }
+    }
+}
+    
